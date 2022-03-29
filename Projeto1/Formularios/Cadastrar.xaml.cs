29918a1b4328b66127556b56ebe00d5e985e747d
OlgaVeronica <olgavas.opo@gmail.com>
2022-03-29 08:38:24 -0400

    adicionei o projeto do vs e o gitignore

---
+++ Projeto1/Formularios/Cadastrar.xaml.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Projeto1.Formularios
+{
+    /// <summary>
+    /// Lógica interna para Cadastrar.xaml
+    /// </summary>
+    public partial class Cadastrar : Window
+    {
+        public static List<string> ListaCadastros = new List<string>();
+        public Cadastrar()
+        {
+            InitializeComponent();
+            Loaded += Cadastrar_Loaded;
+        }
+
+        private void Cadastrar_Loaded(object sender, RoutedEventArgs e)
+        {
+       
+        }
+
+        private void btLogar_Click(object sender, RoutedEventArgs e)
+        {
+            string email = txtEmail.Text;
+            string endereco = txtEndereco.Text;
+            string telefone = txtTelefone.Text;
+            string usuario = txtNomeUsuario.Text;
+            string senhaCadastro = pwbSenhaCadastrar.Password.ToString();
+            string repetirSenhaCad = pwbRepetirSenha.Password.ToString();
+            if (String.IsNullOrWhiteSpace(email) || String.IsNullOrWhiteSpace(endereco)
+            || String.IsNullOrWhiteSpace(telefone) || String.IsNullOrWhiteSpace(usuario.Trim())
+            || String.IsNullOrWhiteSpace(senhaCadastro) || String.IsNullOrWhiteSpace(repetirSenhaCad))
+                MessageBox.Show("Há informações sem preencher, preencha e tente novamente!", "PDS - 2022, 1º Bimestre", MessageBoxButton.OK, MessageBoxImage.Error);
+            else
+            {
+                if (senhaCadastro == repetirSenhaCad)
+                {
+                    string usuarios = $"{txtNomeUsuario.Text}, {pwbSenhaCadastrar.Password.ToString()}";
+                    MessageBox.Show("Usuáro cadastrado com sucesso!", "PDS - 2022, 1º Bimestre", MessageBoxButton.OK, MessageBoxImage.Information);
+                    ListaCadastros.Add(usuarios);
+                    txtEmail.Clear();
+                    txtEndereco.Clear();
+                    txtTelefone.Clear();
+                    txtNomeUsuario.Clear();
+                    pwbSenhaCadastrar.Clear();
+                    pwbRepetirSenha.Clear();
+                    this.Close();
+                }
+                else
+                    MessageBox.Show("Os dados de palavra-passe não coincidem!", "PDS - 2022, 1º Bimestre", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private void btFechar_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btChamarTelaCadastro_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow mainWindow = new MainWindow();
+            mainWindow.Show();
+            this.Close();
+        }
+    }
+}
---
+++ Projeto1/MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Projeto1.Formularios;
+
+namespace Projeto1
+{
+    /// <summary>
+    /// Interação lógica para MainWindow.xam
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        List<string> listaLogin = Cadastrar.ListaCadastros;
+        public MainWindow()
+        {
+            InitializeComponent();
+            Loaded += MainWindow_Loaded;
+        }
+
+        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+      
+        }
+
+        private void btFechar_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btLogar_Click(object sender, RoutedEventArgs e)
+        {
+            string login = txtLogin.Text;
+            string senha = pwbSenha.Password.ToString();
+
+            string usuarios = $"{txtLogin.Text}, {pwbSenha.Password.ToString()}";
+            if(listaLogin.Contains(usuarios))
+                MessageBox.Show($"Usuário '{login}' logado com sucesso!", "PDS - 2022, 1º Bimestre", MessageBoxButton.OK, MessageBoxImage.Information);
+
+            else
+                MessageBox.Show($"O usuário ou a senha está incorreta ou não existe! Tente novamente", "PDS - 2022, 1º Bimestre", MessageBoxButton.OK, MessageBoxImage.Error);
+        }
+
+        private void btChamarTelaCadastro_Click(object sender, RoutedEventArgs e)
+        {
+            Cadastrar TelaCadastro = new Cadastrar();
+            TelaCadastro.ShowDialog();
+        }
+    }
+}
